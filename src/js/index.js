e145d95c187f8afea6053640a0541672cd344ec7
Alexander <alexander.work.mailbox@gmail.com>
2023-05-05 22:36:37 +0600

    Added header

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,17 @@
 window.addEventListener("load", () => {
 	document.querySelector("#email-form__email").addEventListener("focus", hideLabelOnFocus);
+
+	const header = document.querySelector(".header");
+	window.addEventListener("scroll", () => {
+		if(window.scrollY > 0) {
+			header.classList.add("header_scroll");
+		}
+		else {
+			header.classList.remove("header_scroll")
+		}
+	})
+
+	document.querySelector(".burger").onclick = openMenu;
 })
 
 function hideLabelOnFocus({target}) {
@@ -13,4 +25,23 @@ function hideLabelOnFocus({target}) {
 	}
 
 	target.addEventListener("blur", returnLabel);
+}
+
+function openMenu() {
+	const burger = document.querySelector(".burger");
+	burger.classList.add("burger_active");
+
+	const menu = document.querySelector(".header__nav");
+	menu.classList.add("header__nav_active");
+
+	document.body.classList.add("lock");
+
+	burger.onclick = () => {
+		burger.classList.remove("burger_active");
+		menu.classList.remove("header__nav_active");
+
+		document.body.classList.remove("lock");
+
+		burger.onclick = openMenu;
+	};
 }
